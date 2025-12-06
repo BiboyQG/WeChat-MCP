1683faa7d6d3ca070ec223751062b9051c389413
Banghao Chi <banghao2@illinois.edu>
2025-12-06 14:06:28 -0600

    chore: update landing page

---
+++ test.py
@@ -0,0 +1,11 @@
+from wechat_mcp.wechat_accessibility import (
+    get_wechat_ax_app,
+    focus_and_type_search,
+    open_chat_for_contact,
+)
+
+# ax_app = get_wechat_ax_app()
+
+# focus_and_type_search(ax_app, "Leo")
+
+open_chat_for_contact("Leo")
